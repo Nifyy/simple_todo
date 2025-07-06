d64ada8e2a9ac78337c52b2e0a5e5fde5badb9e4
Nifyy <nifsam@outlook.com>
2025-07-06 03:08:55 +0000

    yes

--- lib/todo_screen.dart
+++ lib/todo_screen.dart
@@ -16,50 +16,66 @@ class _TodoScreenState extends State<TodoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple.shade50, // Light purple background
       appBar: AppBar(title: const Text('Todo List')),
-      body: todos.isEmpty
-          ? const Center(
-              child: Text(
-                'There are no TODOS, click the plus button to add one.',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
-                ),
-                textAlign: TextAlign.center,
+body: todos.isEmpty
+    ? Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.inbox,
+              size: 64,
+              color: Colors.purple,
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'There are no TODOS, click the plus button to add one.',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
               ),
-            )
-          : Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: todos.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(todos[index].title),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const DetailScreen(),
-                                settings: RouteSettings(
-                                  arguments: todos[index],
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                    ),
-                  ),
-                ],
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      )
+    : Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: todos.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(todos[index].title),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const DetailScreen(),
+                          settings: RouteSettings(
+                            arguments: todos[index],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
               ),
             ),
+          ],
+        ),
+      ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _navigateAndDisplayAddTodo(context);
         },
+        backgroundColor: Colors.purple.shade100, // Light purple FAB
+        foregroundColor: Colors.black, // Icon color
+        elevation: 6, // Shadow
         child: const Icon(Icons.add),
       ),
     );
